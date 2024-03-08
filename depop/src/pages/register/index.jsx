98352a89863f2f4506a4fcd7a700c1a7423efe54
masdefry <muhammaddefryan@gmail.com>
2024-03-08 10:38:40 +0700

    Dirty & isValid

--- depop/src/pages/register/index.jsx
+++ depop/src/pages/register/index.jsx
@@ -1,4 +1,13 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const registerSchema = Yup.object().shape({
+    firstName: Yup.string().required('Firstname is Required!'),
+    lastName: Yup.string().required('Firstname is Required!'),
+    email: Yup.string().email('Email Must be Valid!').required('Email is Required'),
+    username: Yup.string().min(6, 'Username Must Have Minimum 6 Characters').required('Username is Required'),
+    password: Yup.string().min(6, 'Password Must Have Minimum 6 Characters').required('Password is Required')
+})
 
 export default function Register(){
     return(
@@ -11,45 +20,87 @@ export default function Register(){
                     username: '', 
                     password: ''
                 }}
+                validationSchema={registerSchema}
                 onSubmit={(values) => {
                     console.log(values)
                 }}
            >
-                <Form>
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text">Firstname</span>
-                        </div>
-                        <Field type="text" name='firstName' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
-                    </label>
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text">Lastname</span>
-                        </div>
-                        <Field type="text" name='lastName' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
-                    </label>
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text">Email</span>
-                        </div>
-                        <Field type="text" name='email' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
-                    </label>
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text">Username</span>
-                        </div>
-                        <Field type="text" name='username' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
-                    </label>
-                    <label className="form-control w-full max-w-xs">
-                        <div className="label">
-                            <span className="label-text">Password</span>
-                        </div>
-                        <Field type="text" name='password' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
-                    </label>
-                    <button type='submit'>
-                        Register
-                    </button>
-                </Form>
+            
+                    {
+                        ({dirty, isValid}) => {
+                            return(
+    
+                                    <Form>
+                                    <label className="form-control w-full max-w-xs">
+                                        <div className="label">
+                                            <span className="label-text font-bold">Firstname</span>
+                                        </div>
+                                        <Field type="text" name='firstName' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                                        <ErrorMessage 
+                                            name='firstName'
+                                            component={'div'}
+                                            style={{ color: 'red' }}
+                                        />
+                                    </label>
+                                    <label className="form-control w-full max-w-xs">
+                                        <div className="label">
+                                            <span className="label-text font-bold">Lastname</span>
+                                        </div>
+                                        <Field type="text" name='lastName' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                                        <ErrorMessage 
+                                            name='lastName'
+                                            component={'div'}
+                                            style={{ color: 'red' }}
+                                        />
+                                    </label>
+                                    <label className="form-control w-full max-w-xs">
+                                        <div className="label">
+                                            <span className="label-text font-bold">Email</span>
+                                        </div>
+                                        <Field type="text" name='email' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                                        <ErrorMessage 
+                                            name='email'
+                                            component={'div'}
+                                            style={{ color: 'red' }}
+                                        />
+                                    </label>
+                                    <label className="form-control w-full max-w-xs">
+                                        <div className="label">
+                                            <span className="label-text font-bold">Username</span>
+                                        </div>
+                                        <Field type="text" name='username' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                                        <ErrorMessage 
+                                            name='username'
+                                            component={'div'}
+                                            style={{ color: 'red' }}
+                                        />
+                                    </label>
+                                    <label className="form-control w-full max-w-xs">
+                                        <div className="label">
+                                            <span className="label-text font-bold">Password</span>
+                                        </div>
+                                        <Field type="text" name='password' placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-none" />
+                                        <ErrorMessage 
+                                            name='password'
+                                            component={'div'}
+                                            style={{ color: 'red' }}
+                                        />
+                                    </label>
+                                    {
+                                        console.log(dirty) // By Default False
+                                    }
+                                    <button type='submit'
+                                        className='btn bg-black text-white w-full rounded-none my-3'
+                                        disabled={!(dirty && isValid)}
+                                    >
+                                        Register
+                                    </button>                    
+                                    </Form>
+   
+                            )
+                        }
+                    }
+
            </Formik>
         </div>
     )
